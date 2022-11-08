3033caaadd9cf244120691c288236217c0df5cd0
JiaJi99 <115800870+JiaJi99@users.noreply.github.com>
2022-11-08 20:17:16 +0800

    Add files via upload

--- MOBLIMA/src/BusinessLayer/PriceLayer.java
+++ MOBLIMA/src/BusinessLayer/PriceLayer.java
@@ -4,16 +4,38 @@ import moblima.customException.PriceException.NegativePriceException;
 
 public class PriceLayer {
 	
-	public static boolean isPriceinValid(double price) {
+	public static boolean isPriceValid(double student,double adult,double senior,double weekend,double after6,double _3D) {
+		if(!isPriceValid(student)) {
+			return false;	
+		}
+		if(!isPriceValid(adult)) {
+			return false;
+		}
+		if(!isPriceValid(senior)) {
+			return false;
+		}
+		if(!isPriceValid(weekend)) {
+			return false;
+		}
+		if(!isPriceValid(after6)) {
+			return false;
+		}
+		if(!isPriceValid(_3D)) {
+			return false;
+		}
+		return true;
+	}
+	
+	public static boolean isPriceValid(double price) {
 		if (price<0){
 			try {
 				throw new NegativePriceException();
 			} catch (NegativePriceException e) {
 				System.out.println(e.getMessage());
 			}
-			return true;
+			return false;
 		}
-		return false;
+		return true;
 	}
 
 }
