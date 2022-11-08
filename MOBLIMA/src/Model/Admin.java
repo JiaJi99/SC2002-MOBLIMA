ec96337287d3fef2b73da36d51816756f3c62ff3
JiaJi99 <115800870+JiaJi99@users.noreply.github.com>
2022-11-08 18:37:07 +0800

    Delete MOBLIMA/src/Model directory

--- MOBLIMA/src/Model/Admin.java
+++
@@ -1,9 +0,0 @@
-package Model;
-
-@SuppressWarnings("serial")
-public class Admin extends User {
-	
-	public Admin(String username, String password) throws NoSuchAlgorithmException {
-		super(username, password, ADMIN);
-	}
-}
