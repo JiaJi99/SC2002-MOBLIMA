1ebc778af17a1a5e7c29b9516289d5544fcb89b3
Dhairya <97079960+dhairyarungta@users.noreply.github.com>
2022-11-09 23:11:13 +0800

    Add files via upload

--- MOBLIMA/src/Boundary/BookBuy.java
+++ MOBLIMA/src/Boundary/BookBuy.java
@@ -1,8 +1,32 @@
+import java.time.LocalDateTime;
+
 public class BookBuy {
-    
+    private CineplexController cinplexCtrl;
+    private CinemasController cinemasController;
+    private MoviesCtrl moviesCtrl;
+    private SessionsCtrl sessionsCtrl;
+    private PriceCtrl priceCtrl;
 
 
-    public void bookbuy(){
+    private String movieName;
+    private String cinemaCode;
+    private LocalDateTime localDateTime;
+    private Cinemas chosenCinema;
+    private Session chosenSession;
+    private SeatingPlan giveSeatPlan;
+    private int numTickets;
+
+    public BookBuy(){
+        this.cinplexCtrl = new CineplexController();
+        this.cinemasController = new CinemasController();
+        this.moviesCtrl = new MoviesCtrl();
+        this.priceCtrl = new PriceCtrl();
+        this.sessionsCtrl = new SessionsCtrl();
+
+    }
+
+
+    public void bookbuyMethod(){
         
     }
 }
