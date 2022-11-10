1e3be67dc35827bd6ed86260eeebfe96ebf7d3a0
JiaJi99 <115800870+JiaJi99@users.noreply.github.com>
2022-11-10 21:56:18 +0800

    added java doc

--- MOBLIMA/src/BusinessLayer/SessionsLayer.java
+++ MOBLIMA/src/BusinessLayer/SessionsLayer.java
@@ -18,43 +18,37 @@ static SessionsCtrl sessionsCtrl = new SessionsCtrl();
 
     
     /** 
-     * Check if session can be created based on parameters passed
+     * Check if session does not conflict with existing sessions based on parameters passed
      * @param cinemaCode        Code of cinema's that this session will be added to
      * @param movie             Movie that this session will be screening
-     * @param sessionDateTime   Date and time which the session will begin screening
-     * @return boolean          Return true if session can be created, else false
+     * @param sessionDateTime   Date and time which the session will begin
+     * @return boolean          Return true if session is valid, else false
      */
     public static boolean isSessionValid(String cinemaCode, Movie movie, LocalDateTime sessionDateTime) {
 
-        // get start and end time of new session
         LocalDate sessionDate = sessionDateTime.toLocalDate();
         LocalTime sessionStartTime = sessionDateTime.toLocalTime();
         LocalTime sessionEndTime = sessionStartTime.plusMinutes((long) (movie.getRunTime()));
 
-        // get all sessions on the date and in the same cinema
         ArrayList<Sessions> allSelectedSessions = sessionsCtrl.readByAttributes(cinemaCode, sessionDate);
         Sessions session_this = null;
         Sessions session_next = null;
 
         if (allSelectedSessions.isEmpty()) {
-            // no session means there's nothing to check and will have no conflict
-            return true;
+        	//no nothing 
+        	return true;
         } 
        
         else {
 
-            // return true if new session ends before earliest session starts
             session_this = allSelectedSessions.get(0);
             if (sessionEndTime.isBefore(session_this.getStartTime())) 
                 return true;
 
-            // return true if new session starts after latest session ends
             session_this = allSelectedSessions.get(allSelectedSessions.size()-1);
             if (sessionStartTime.isAfter(session_this.getEndTime())) 
                 return true;
 
-            // loop through and compare with all sessions on that day and cinema
-            // no need to check last item (checked by if-statement above)
             for (int i=0; i<(allSelectedSessions.size()-1); i++) {
                 session_this = allSelectedSessions.get(i);
                 session_next = allSelectedSessions.get(i+1);
