eb38d9d3090b83534b85fa191a2b245bd3483c07
JiaJi99 <115800870+JiaJi99@users.noreply.github.com>
2022-11-10 11:35:43 +0800

    Update AccountType.java
    Java.doc added

--- MOBLIMA/src/model/AccountType.java
+++ MOBLIMA/src/model/AccountType.java
@@ -1,6 +1,16 @@
 package moblima.Model;
 
+/** 
+ * AccountType that can be used
+ */
 public enum AccountType {
+	/**
+	 * Admin AccountType
+	 */
 	ADMIN,
+	
+	/**
+	 * Moviergoer Account Type
+	 */
 	MOVIEGOER;
 }
