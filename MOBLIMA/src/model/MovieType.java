aca61a82d81c97b0004dab494aa66747c63d4d01
JiaJi99 <115800870+JiaJi99@users.noreply.github.com>
2022-11-10 14:41:46 +0800

    Update MovieType.java

--- MOBLIMA/src/model/MovieType.java
+++ MOBLIMA/src/model/MovieType.java
@@ -1,14 +1,38 @@
 package moblima.Model;
 
+/**
+ * Movie type to be used
+ */
 public enum MovieType {
-	IN_2D("2D"),
-	IN_3D("3D"),
-	BLOCKBUSTER("Blockbuster");
 	
+	/**
+	 * 2D
+	 */
+	IN_2D("2D"),
+	
+	
+	/**
+	 * 3D
+	 */
+	IN_3D("3D");
+	
+	/**
+	 * Movietype type
+	 */
 	private final String type;
+	
+	/**
+	 * Construct enum
+	 * @param type Movietype
+	 */
 	private MovieType(String type) {
 		this.type = type;
 	}
+	
+	/**
+	 * Get enum in string
+	 * @return String type
+	 */
 	public String getType() {
 		return type;
 	}
