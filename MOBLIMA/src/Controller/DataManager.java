0b93783f58722148d30d5f1dafc6e76c4b4ff9bc
JiaJi99 <115800870+JiaJi99@users.noreply.github.com>
2022-11-11 16:12:35 +0800

    Update DataManager.java

--- MOBLIMA/src/Controller/DataManager.java
+++ MOBLIMA/src/Controller/DataManager.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 import moblima.Model.*;
 
 public class DataManager implements Serializable{
-	private static final String FILENAME_ADMIN = "C:\\Users\\kenny\\OneDrive\\Documents\\MOBLIMA\\AdminData.txt";
-	private static final String FILENAME_MOVIEGOER = "C:\\Users\\kenny\\OneDrive\\Documents\\MOBLIMA\\MovieGoerData.txt";
-	private static final String [] FILES = {FILENAME_ADMIN, FILENAME_MOVIEGOER};
+	public static final String FILENAME_ADMIN = "C:\\Users\\kenny\\OneDrive\\Documents\\MOBLIMA\\AdminData.txt";
+	public static final String FILENAME_MOVIEGOER = "C:\\Users\\kenny\\OneDrive\\Documents\\MOBLIMA\\MovieGoerData.txt";
+	public static final String [] FILES = {FILENAME_ADMIN, FILENAME_MOVIEGOER};
 	private ArrayList<AdminAccount> adminAccounts;
 	private ArrayList<MovieGoerAccount> movieGoerAccounts;
 	
