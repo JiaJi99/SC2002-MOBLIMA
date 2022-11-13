e93849e3be3cdc3360d545f5cba2ebf8ddef5781
Cankneeee <95756602+Cankneeee@users.noreply.github.com>
2022-11-13 21:11:54 +0800

    Add files via upload

---
+++ Javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"BaseClasses"},{"l":"ExceptionClasses"},{"l":"ManagerClasses"},{"l":"ValidInput"}];updateSearchResults();
